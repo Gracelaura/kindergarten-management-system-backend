d768efcb4d1493c16de7987320e287b625de62b0
Gracelaura <gracenjuguna05@gmail.com>
2022-12-22 17:25:39 +0300

    added index, show and create routes in the parents and teachers controllers as well as validations in the respective models

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,25 @@
 class TeachersController < ApplicationController
+    def index
+        teachers = Teacher.all
+        render json: teachers, status: :ok
+    end
+
+    def show
+        teacher = find_teacher
+        render json: teacher, status: :ok
+    end
+    
+    def create
+        teacher = Teacher.create(teacher_params)
+        render json: teacher, status: :created
+    end
+
+    private
+    def find_teacher
+        Teacher.find_by(id: params[:id])
+    end
+
+    def teacher_params
+        params.permit(:fullname, :career_name, :phone_number, :password, :role, :email, :gender)
+    end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,12 @@ class Teacher < ApplicationRecord
     has_many :grades
     has_many :students, through: :grades
     has_many :attendances
+
+    validates :email, presence: true, uniqueness: true
+    validates :fullname, presence: true
+    validates :career_name, presence: true
+    validates :password, length: {minimum: 5}
+    validates :phone_number, length: {maximum: 10}
+    validates :gender, presence: true
+    validates :role, presence: true
 end
