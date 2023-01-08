aba402733a5af1d3d19273fba36212f6a7d88fcc
Muriithi <muriithi.kibata@student.moringaschool.com>
2023-01-08 22:10:59 +0300

    DOne

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -37,6 +37,6 @@ class StudentsController < ApplicationController
         end
 
         def record_invalid invalid
-    render json: {errors : invalid.record.errors.full_messages}, status: :unprocessable_entity
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
         end
 end
