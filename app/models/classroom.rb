77b33c5888b45d2a11171eb7d3516f7aa75eaa8e
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-01-18 18:51:32 +0300

    KIN-75 last push

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -3,5 +3,6 @@ class Classroom < ApplicationRecord
     has_many :students, dependent: :destroy
     has_many :attendances, dependent: :destroy 
     has_many :parents, through: :students
+    has_many :disciplines, through: :students
     validates :teacher_id, presence: true
 end
